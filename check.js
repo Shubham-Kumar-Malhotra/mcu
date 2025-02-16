500aaf2faea644440981909475041bc4b0b06d50
Shubham-Kumar-Malhotra <shubhamkumar4600@gmail.com>
2025-02-16 14:43:48 +0530

    code writen by subh

--- check.js
+++ check.js
@@ -1,4 +1,12 @@
-function check(num){
-    
+function check(number){
+    if(number%2==0){
+        return true;
+    }else{
+        return false;
+    }
 }
-check(5);
+if(check(5) == true){
+    console.log("even number");
+}else{
+    console.log("odd numbeer");
+}
